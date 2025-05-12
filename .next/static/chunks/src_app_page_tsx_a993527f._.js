3fcc8a42a88b3910772780965babaabc0a753434
Houssem Haddad <63915518+househd@users.noreply.github.com>
2025-05-12 19:07:58 +0100

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747073278018

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_506412e1._.js",
+  "static/chunks/src_b6fe927d._.js"
+],
+    source: "dynamic"
+});
